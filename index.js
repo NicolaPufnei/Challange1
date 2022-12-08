0bbd234209bf55b982b6436b6b0c4afe7c923296
NicolaPufnei <nicolapufnei@gmail.com>
2022-12-08 22:07:49 +0200

    v final

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const menu_btn = document.querySelector('.hamburger');
 const menu_options = document.querySelector('.mobile-nav');
+const blurul = document.querySelector('.blur');
 
 menu_btn.addEventListener('click' , () => {
         menu_btn.classList.toggle('is-active');
         menu_options.classList.toggle('is-active');
+        blurul.classList.toggle('is-active');
     })
