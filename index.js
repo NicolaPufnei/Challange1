78809c0141a0f1b6c5e325cf2520fab963da06df
NicolaPufnei <nicolapufnei@gmail.com>
2022-12-08 21:42:07 +0200

    V1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const menu_btn = document.querySelector('.hamburger');
 const menu_options = document.querySelector('.mobile-nav');
 
-menu_btn.addEventListener('click' , function() {
-    menu_btn.classList.toggle('is-active');
-    menu_options.classList.toggle('is-active');
-})
+menu_btn.addEventListener('click' , () => {
+        menu_btn.classList.toggle('is-active');
+        menu_options.classList.toggle('is-active');
+    })
